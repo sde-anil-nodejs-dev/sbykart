8793396603962030fad852442f011669a9e86923
Anil Yadav <iter.anilyadav@gmail.com>
2020-07-17 13:13:00 +0530

    sbykart.com

---
+++ UserInterface/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>React APP is Working..626</p>
+    </div>
+  );
+}
+
+export default App;
