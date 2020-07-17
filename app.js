3f3396a6a3d9986cb9ec2c898a722d49eec693eb
Anil Yadav <iter.anilyadav@gmail.com>
2020-07-17 13:39:43 +0530

    add

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var kartRouter = require('./routes/kart');
 
 var app = express();
 
@@ -19,8 +18,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', kartRouter);
+//
+// react render
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('UserInterface/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'UserInterface', 'index.html')); // relative path
+  });
+}
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
