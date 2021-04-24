2ce3f986bf81b510ab116858b8a28fe4c5e72954
Anil Yadav <iter.anilyadav@gmail.com>
2021-04-25 00:33:29 +0530

    updated

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
+  // set locals, only providing error in development j
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
